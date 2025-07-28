92af82f747bb12a286b3ef3dd67712ccf3dedb39
Md. Irfan Khan <learn.irfankhan@gmail.com>
2025-07-28 14:15:29 +0600

    card added

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,43 @@
+
+import { AiOutlineStar } from 'react-icons/ai';
+const Card = ({book}) => {
+    const {bookName,author,image,tags,category,rating}=book;
+    return (
+        <div className="card bg-base-100 w-96 shadow-sm px-3 pt-3">
+            <figure className="bg-base-300 py-3 ">
+                <img className="h-30"
+                    src={image}
+                    alt="Shoes" />
+            </figure>
+            <div className="card-body px-3">
+                <div className="card-actions justify-start">
+                    {/* <div className="badge badge-outline">Fashion</div> */}
+                   
+                    {
+                        tags.map((tag,idx)=> <div key={idx} className="badge text-green-700 bg-[#22be0a2f] badge-outline">{tag}</div>)
+                    }
+                </div>
+                <h2 className="card-title">
+                    {bookName}
+                    <div className="badge badge-secondary">NEW</div>
+                </h2>
+                <p className="font-bold">By: {author}</p>
+
+                <div className="border border-b-gray-300 opacity-5 my-2.5 border-dashed"></div>
+
+                <div className='flex justify-between items-center font-medium opacity-[0.8]'>
+                    <div>
+                        {category}
+                    </div>
+                    <div className='flex items-center gap-1'>
+                       <span>{rating}</span>
+                       <AiOutlineStar className='h-5 w-5'></AiOutlineStar>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    );
+};
+
+export default Card;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,23 @@
 
+import { useEffect, useState } from 'react';
 import Banner from '../Banner/Banner'
+import Card from '../Card/Card';
 const Home = () => {
+    const [books, setBooks] = useState([])
+    useEffect(() => {
+        fetch('api/booksData.json').
+            then(res => res.json()).then(data => setBooks(data))
+    }, [])
+    console.log(books);
+
     return (
         <div>
             <Banner></Banner>
-        
+            <div className='grid grid-cols-1 lg:grid-cols-3 gap-y-2'>
+                {
+                    books.map(book => <Card book={book}></Card>)
+                }
+            </div>
         </div>
     );
 };
