b0e658d545a6370443c2f9c61b319820f136faac
Md. Irfan Khan <learn.irfankhan@gmail.com>
2025-07-28 11:37:34 +0600

    nav added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 
+import { Outlet } from 'react-router'
 import './App.css'
+import Nav from './components/Nav/Nav'
 
 function App() {
   
 
   return (
     <>
-    
+    <Nav></Nav>
+    <Outlet></Outlet>
     </>
   )
 }
